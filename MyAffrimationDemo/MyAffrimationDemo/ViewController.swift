7a33b8365f86a084cdf301e57be6601423c4abf2
Oreoluwa2 <68120694+Oreoluwa2@users.noreply.github.com>
2020-07-13 23:36:37 -0500

    Add files via upload

---
+++ MyAffrimationDemo/MyAffrimationDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAffrimationDemo
+//
+//  Created by Oreoluwa Omomofe on 7/12/20.
+//  Copyright © 2020 One Summer Chicago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
